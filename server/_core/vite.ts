60b404e15a8a5af7733198c28a491e45a33204d9
drwesleycamara2 <drwesleycamara2@gmail.com>
2026-04-03 00:33:11 -0300

    fix: remove vite.config.ts static import from server to prevent CJS bundling issues

--- server/_core/vite.ts
+++ server/_core/vite.ts
@@ -4,7 +4,7 @@ import { type Server } from "http";
 import { nanoid } from "nanoid";
 import path from "path";
 import { createServer as createViteServer } from "vite";
-import viteConfig from "../../vite.config";
+
 
 export async function setupVite(app: Express, server: Server) {
   const serverOptions = {
@@ -14,8 +14,8 @@ export async function setupVite(app: Express, server: Server) {
   };
 
   const vite = await createViteServer({
-    ...viteConfig,
-    configFile: false,
+    
+    
     server: serverOptions,
     appType: "custom",
   });
